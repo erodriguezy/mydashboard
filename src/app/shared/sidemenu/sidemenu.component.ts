746b39d3f72cb59cc1b1190be0b0cb73af01891e
Eduardo Rodriguez Yapur <duduyapur@gmail.com>
2024-04-01 09:18:25 -0400

    Implementacion de sidebar con un error pendiente de revision

---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './sidemenu.component.html',
+  styleUrl: './sidemenu.component.css',
+
+})
+export class SidemenuComponent {
+
+  public menuItems = routes
+  .map((route) => route.children ?? [])
+  .flat()
+  .filter((route) => route && route.path)
+  .filter((route) => !route.path?.includes(':'));
+
+  constructor(){
+  }
+
+}
